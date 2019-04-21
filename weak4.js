f48feb4322fdf2e9da3d24e6499082288f339af9
nitishjha28 <42953102+nitishjha28@users.noreply.github.com>
2019-04-22 03:11:25 +0530

    weak4 assignment

---
+++ weak4.js
@@ -0,0 +1,9 @@
+var name=["John","Nitish","Jack"];
+name.forEach(function(names){
+      /*console.log(names)*/
+if((names[0]=='J')||(names[0]=='j'))
+console.log("Goodbye "+names)
+else
+console.log("Hello "+names)
+}
+);
